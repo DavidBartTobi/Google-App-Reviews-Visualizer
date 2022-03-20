c18ea68180617a06bf4c2dfaf626ea0738d70b3f
DavidBartTobi <82588072+DavidBartTobi@users.noreply.github.com>
2022-03-20 21:55:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import gui
 
 def main():
     root = Tk()
-    gui.Window_Style(root, "Google App Reviews Visualizer", "538x538")
+    gui.window_style(root, "Google App Reviews Visualizer", "538x538")
 
     image = Image.open('logos/logo.jpg')
     image.thumbnail((538, 538))
     resized_image = ImageTk.PhotoImage(image)
-    img_label = Label(root, image=resized_image, bg=gui.Main_Theme_Color())
+    img_label = Label(root, image=resized_image, bg=gui.main_theme_color())
     img_label.grid(row=0, column=0, rowspan=9, columnspan=3)
 
     Program(root)
